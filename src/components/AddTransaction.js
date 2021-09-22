097e5604a77794d4921581a1eb3cb7058c8ca5ee
Vi2x101 <65918376+Vi2x101@users.noreply.github.com>
2021-09-22 12:44:03 -0400

    added global states

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000000000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
             type="type"
             value={text}
             onChange={(e) => setText(e.target.value)}
+            id="text"
             placeholder="Enter text..."
           />
         </div>
@@ -26,11 +42,10 @@ export const AddTransaction = () => {
             type="number"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-            id="amount"
             placeholder="Enter amount..."
           />
         </div>
-        <button class="btn">Add transaction</button>
+        <button className="btn">Add transaction</button>
       </form>
     </>
   );
